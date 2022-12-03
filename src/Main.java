7676ed32cc471938a274c706f6add3493a83c0b0
Neto study <daria.bugaevskaya@gmail.com>
2022-12-03 15:29:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 20_000;
+        int bonus = 20;
+
+        System.out.println("Бонусные мили");
+        System.out.println(price / bonus);
+    }
+}
